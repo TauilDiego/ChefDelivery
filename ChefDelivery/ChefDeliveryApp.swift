de0d721774d1e688d0ddf3fa05f93c1ab67748ba
Macbook Pro de Diego <tauildiego@icloud.com>
2024-10-26 17:36:58 -0300

    Initial Commit

---
+++ ChefDelivery/ChefDeliveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChefDeliveryApp.swift
+//  ChefDelivery
+//
+//  Created by Diego Tauil Sousa on 26/10/24.
+//
+
+import SwiftUI
+
+@main
+struct ChefDeliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
