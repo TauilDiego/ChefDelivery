ea8d1fcd08fae0c0395fc9a2d808a600bb488439
Macbook pro de Diego <tauildiego@icloud.com>
2025-01-16 09:44:02 -0300

    feat: design implementation

---
+++ ChefDelivery/Model/ProductType.swift
@@ -0,0 +1,8 @@
+//
+//  ProductType.swift
+//  ChefDelivery
+//
+//  Created by Diego Tauil Sousa on 11/11/24.
+//
+
+import Foundation
---
+++ ChefDelivery/ProductView/ProductDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  ProductDetailView.swift
+//  ChefDelivery
+//
+//  Created by Diego Tauil Sousa on 16/11/24.
+//
+
+import SwiftUI
+
+struct ProductDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProductDetailView()
+}
